c2707e0029d44800db85ca88b2e9c34d265f804e
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-22 16:45:58 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,21 @@ const route = Router();
 const not_found = require('./404');
 const int_error = require('./500');
 
+/**
+* After Watch RP Loading Screens
+*/
+const loading_afterwatch_main = require('./v1/loading/afterwatch/main');
+
+/**
+* ToxicFX Loading Screens
+*/
+const loading_toxicfx_testing = require('./v1/loading/toxicfx/testing');
+
 route.use('/404', not_found);
 route.use('/500', int_error);
+
+route.use('/v1/loading/awrp', loading_afterwatch_main);
+route.use('/v1/loading/toxicfx', loading_toxicfx_testing);
 
 route.get('/', (req, res) => {
 
