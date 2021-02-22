9aa158217eb84aaa7e652c6e24d08a6e3999c8bf
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-22 16:42:17 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@
 const { Router } = require("express");
 const route = Router();
 
+const not_found = require('./404');
+const int_error = require('./500');
+
+route.use('/404', not_found);
+route.use('/500', int_error);
+
 route.get('/', (req, res) => {
 
   res.status('200').send(JSON.stringify({ 
