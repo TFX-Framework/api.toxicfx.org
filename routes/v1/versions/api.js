d57b2fb24bc9cce7f99257b8ac1e0ae5f398112d
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-22 17:31:01 -0700

    Update api.js

--- routes/v1/versions/api.js
+++ routes/v1/versions/api.js
@@ -1,1 +1,28 @@
+const { Router } = require("express");
+const route = Router();
 
+
+route.get('/', (req, res) => {
+
+    let project_name = 'ToxicFX Website'
+
+    let current_version = '1.0.0'
+
+    let previous_version = '1.0.0'
+
+    let newest_version = '1.0.0'
+
+    let stable_version = '1.0.0'
+
+    let data = {
+        name: project_name,
+        currentVersion: current_version,
+        previousVersion: previous_version,
+        newestVersion: newest_version,
+        stableVersion: stable_version
+    }
+
+    res.status(200).send(JSON.stringify(data))
+});
+
+module.exports = route;
