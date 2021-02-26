e19a93bc4e4255a02dac6b454e924c94eae311d4
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-26 03:23:38 -0700

    Update community-cad.js

--- routes/v1/versions/community-cad.js
+++ routes/v1/versions/community-cad.js
@@ -1,1 +1,28 @@
+const { Router } = require("express");
+const route = Router();
 
+
+route.get('/', (req, res) => {
+
+    let project_name = 'ToxicFX Community CAD'
+
+    let current_version = '2.15.0'
+
+    let previous_version = '2.15.0'
+
+    let newest_version = '2.15.0'
+
+    let stable_version = '2.15.0'
+
+    let data = {
+        name: project_name,
+        currentVersion: current_version,
+        previousVersion: previous_version,
+        newestVersion: newest_version,
+        stableVersion: stable_version
+    }
+
+    res.status(200).send(JSON.stringify(data))
+});
+
+module.exports = route;
