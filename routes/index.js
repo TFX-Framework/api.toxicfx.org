e9818bbb6829299002a0b6d1891774121d582d1f
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 20:55:24 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,5 +15,29 @@ route.get('/', (req, res) => {
   res.redirect('')
 });
 
+route.get('/inv', (req, res) => {
+  res.redirect('res.redirect(301, 'https://discord.com/api/oauth2/authorize?client_id=835997853263462461&permissions=2617638135&scope=bot')
+});
+
+route.get('/invite', (req, res) => {
+  res.redirect('res.redirect(301, 'https://discord.com/api/oauth2/authorize?client_id=835997853263462461&permissions=2617638135&scope=bot')
+});
+
+route.get('/git', (req, res) => {
+  res.redirect('res.redirect(301, 'https://github.com/TheRealToxicDev/DisPurge')
+});
+
+route.get('/github', (req, res) => {
+  res.redirect('res.redirect(301, 'https://github.com/TheRealToxicDev/DisPurge')
+});
+
+route.get('/discord', (req, res) => {
+  res.redirect('res.redirect(301, 'https://discord.gg/tSDjeTyzUj')
+});
+
+route.get('/support', (req, res) => {
+  res.redirect('res.redirect(301, 'https://discord.gg/tSDjeTyzUj')
+});
+
 
 module.exports = route;
